34c2d18e554ced51f6e3d0eb4827c080b3acb2ef
mantasarul <mantasarul.elahi@northsouth.edu>
2021-07-20 20:55:27 +0600

    navigation fixed

--- table_app/urls.py
+++ table_app/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view()),
-    path('add-category', views.AddCategoryView.as_view()),
-    path('add-link', views.AddLinkView.as_view()),
-    path('all-categories', views.AllCategoriesView.as_view()),
-    path('all-links', views.AllLinksView.as_view()),
+    path('add-category', views.AddCategoryView.as_view(), name="add-category"),
+    path('add-link', views.AddLinkView.as_view(), name="add-link"),
+    path('all-categories', views.AllCategoriesView.as_view(), name='all-categories'),
+    path('all-links', views.AllLinksView.as_view(), name='all-links'),
     # path('delete-category/<int:id>', views.deleteCategory),
     path('delete-category/<int:id>', views.DeleteCategoryView.as_view()),
     path('edit-category/<int:id>', views.editCategory),
